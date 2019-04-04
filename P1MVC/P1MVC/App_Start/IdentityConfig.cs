a84e83270529adda33d599137ddc74da72873abc
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-03 23:37:36 -0400

    commit con modelo en MVC

--- P1MVC/P1MVC/App_Start/IdentityConfig.cs
+++ P1MVC/P1MVC/App_Start/IdentityConfig.cs
@@ -54,7 +54,7 @@ namespace P1MVC
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
---
+++ P1MVC/P1MVC/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace P1MVC.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<P1MVC.Models.Product> Products { get; set; }
+    }
+}
